c8bba2b1c194e05a2e8d22693b395b673a1fadd9
Ville Hytönen <villeahy@metropolia.fi>
2018-02-27 10:26:09 +0200

    added front

--- backend.js
+++ backend.js
@@ -4,3 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 require('./db');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'client/build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extemded: false}));
+
+app.use(cors());
+
+const port = process.env.PORT||3420;
+
+app.listen(port, () => console.log('server started on port '+port));
