b900a64d6f1d85de03e9bf38ce2e03e6d57cc86e
romppane <ronim@metropolia.fi>
2018-03-07 21:31:00 +0200

    User taulu ja uusia komentoja /user:iin

--- Routes/user.js
+++ Routes/user.js
@@ -3,16 +3,60 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 
-//Palauttaa tyhjää???
+// Pitää tehdä oma message model ja joku tapa viitata viesteihin.
+// User voi hakea omat viestit hyödyntäen tokenia?
+// Uuden viestin kirjoitukseen tokenin tarkastus!
+// Käyttäjän poistoon tokenin tarkistus, jwt.verifyn avulla poisto!
+// Viestin poistoon tokenin tarkistus, jwt.verifyn avulla poisto!
+
+
 router.post('/login', function(req, res) {
-  Post.find({'username' : req.body.username}).findOne({"password" : req.body.password}).then(user => handleLogin(req, res, user))
+  User.find({'username' : req.body.username}).findOne({"password" : req.body.password}).then(user => handleLogin(req, res, user))
+});
+
+// User taulun get all
+router.get('/all', (req, res) => {
+  User.find().then(results => res.json({
+    results
+  }))
+});
+
+// uusi käyttäjä User tauluun, reitti voi muuttua!
+router.post('/newUser', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  }
+  new User(newUser).save();
+  res.json({
+    success: true, message: "New user created"
+  })
 })
 
+// verifyToken testi
 router.get('/pena', verifyToken, function(req, res) {
   res.send({
     pena: "pena"
+  })
+})
+
+
+// jwt.verify testaamista, authData pitää sisällään tokenin omistajan tiedot kuten kuuluukin
+router.get('/posts', verifyToken, function(req, res) {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    }
+    else {
+      res.json({
+        message: 'Information fetched',
+        authData
+      })
+    }
   })
 })
 
